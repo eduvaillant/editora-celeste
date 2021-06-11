469be8a5688a56b3039669244dde998b979ecc49
Elian Batista <elianbatista2000@gmail.com>
2021-06-11 19:36:25 -0300

    criacao inicial do projeto

---
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/EcApplication.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcApplication.class, args);
+	}
+
+}
---
+++ codigos/ec/ec/src/test/java/edu/ifes/ci/si/les/ec/EcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
