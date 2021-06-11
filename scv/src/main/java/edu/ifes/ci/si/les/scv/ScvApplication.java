4f8053faabd65ecded4e087cd34df7cd33034350
Lucas Gobbi <llcostagobbi@gmail.com>
2021-06-11 18:53:43 -0300

    Criando projeto no spring tools suite

---
+++ scv/src/main/java/edu/ifes/ci/si/les/scv/ScvApplication.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.scv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScvApplication.class, args);
+	}
+
+}
---
+++ scv/src/test/java/edu/ifes/ci/si/les/scv/ScvApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifes.ci.si.les.scv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
