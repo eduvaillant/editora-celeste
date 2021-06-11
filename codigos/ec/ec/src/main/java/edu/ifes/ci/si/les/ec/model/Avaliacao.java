72321d74eba52a777db55cbaec086a83f04099a3
Elian Batista <elianbatista2000@gmail.com>
2021-06-11 20:40:09 -0300

    classes

--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Avaliacao.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Avaliacao.java
@@ -1,15 +1,34 @@
 package edu.ifes.ci.si.les.ec.model;
 
-public class Avaliacao {
+import java.io.Serializable;
 
+import javax.persistence.*;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@Entity
+public class Avaliacao implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private Integer nota;
 
 	private Integer comentario;
 
+	@ManyToOne
+	@JoinColumn(name="livro_id")
 	private Livro livro;
 
+	@ManyToOne
+	@JoinColumn(name="usuario_id")
 	private Usuario usuario;
 
 }
