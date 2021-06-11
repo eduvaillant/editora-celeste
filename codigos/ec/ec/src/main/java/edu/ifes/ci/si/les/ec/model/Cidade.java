e5d07696a0f4499c955002ccac1ccefd29cafe8a
Lucas Gobbi <llcostagobbi@gmail.com>
2021-06-11 20:33:03 -0300

    Atividade 7-Cidade

--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Cidade.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Cidade.java
@@ -1,13 +1,27 @@
 package edu.ifes.ci.si.les.ec.model;
 
+import java.io.Serializable;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@Entity
 public class Cidade {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String nome;
 
-	private Bairro bairro;
+	private Bairro bairro; 
 
+	@ManyToOne
+	@JoinColumn(name="uf_id")
 	private UF uf;
 
 }
