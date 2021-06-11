8e5c43ef2cd572d2416bca51d321bdb3de4cf7de
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2021-06-11 20:32:10 -0300

    atividade 7 - post, tipoDeAssinatura, tipoDeLivro

--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Post.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Post.java
@@ -1,11 +1,30 @@
 package edu.ifes.ci.si.les.ec.model;
 
-public class Post {
+import java.io.Serializable;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+
+@Entity
+public class Post implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(length = 150)
+	@NotBlank(message = "Preencha a descrição")
+	@Size(min = 2, max = 150, message = "A descrição precisa ter no mínimo 2 letras")
 	private String descricao;
 
+	@ManyToOne
+	@JoinColumn(name="escritor_id")
 	private Escritor escritor;
 
 }
--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/TipoDeAssinatura.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/TipoDeAssinatura.java
@@ -1,13 +1,35 @@
 package edu.ifes.ci.si.les.ec.model;
 
-public class TipoDeAssinatura {
+import java.io.Serializable;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+
+@Entity
+public class TipoDeAssinatura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(length = 100)
+	@NotBlank(message = "Preencha o nome do tipo de assinatura")
+	@Size(min = 2, max = 100, message = "O nome do tipo da assinatura precisa ter no mínimo 2 letras")
 	private String nome;
 
+	@Column(length = 10)
+	@NotBlank(message = "Preencha o valor do plano")
+	@Size(min = 1, max = 150, message = "O valor do plano não pode ser grátis")
 	private double valor;
 
+	@ManyToOne
+	@JoinColumn(name="assinatura_id")
 	private Assinatura[] assinatura;
 
 }
