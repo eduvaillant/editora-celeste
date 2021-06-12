b9413d4bad76bdf53b5b09f9d934b76433f3a150
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-12 09:36:55 -0300

    add decorators na classe Livro

--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Livro.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Livro.java
@@ -1,25 +1,56 @@
 package edu.ifes.ci.si.les.ec.model;
 
-public class Livro {
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import lombok.*;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class Livro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(length = 150)
+  @NotBlank(message = "Preencha o Título")
+  @Size(min = 2, max = 150, message = "A titulo precisa ter no mínimo 2 letras e no máximo 150")
 	private String titulo;
 
+	@Column(length = 150)
+	@NotBlank(message = "Preencha o Subtítulo")
+	@Size(min = 2, max = 150, message = "A subtítulo precisa ter no mínimo 2 letras e no máximo 150")
 	private String subtitulo;
 
+	@Column(length = 255)
+	@NotBlank(message = "Preencha a Descrição")
+	@Size(min = 2, max = 255, message = "A descrição precisa ter no mínimo 2 letras e no máximo 255")
 	private String descricao;
 
+	@NotNull(message = "A quantidade de capítulos deve ser preenchida")
 	private Integer qtd_capitulos;
 
+	@NotNull(message = "O número de páginas deve ser preenchido")
 	private Integer numero_paginas;
 
+	@NotNull(message = "O Escritor deve ser preenchido")
+  @OneToMany()
+  @JoinColumn(name = "escritor_id")
 	private Escritor escritor;
 
+	@NotNull(message = "O Tipo de Livro deve ser preenchido")
+  @ManyToOne()
+  @JoinColumn(name = "tipo_de_livro_id")
 	private TipoDeLivro tipoDeLivro;
 
+	@NotNull(message = "O Livro deve ter no mínimo um Capítulo")
+  @OneToMany()
+  @JoinColumn(name = "capitulo_id")
 	private Capitulo[] capitulo;
-
-	private Avaliacao avaliacao;
-
 }
