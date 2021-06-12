e6703dc33287f97abbe53a5a323f97c0ab344aed
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-12 14:32:26 -0300

    add FavoritoRepository

---
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/repositories/FavoritoRepository.java
@@ -0,0 +1,11 @@
+package edu.ifes.ci.si.les.ec.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.ifes.ci.si.les.ec.model.Favorito;
+
+@Repository
+public interface FavoritoRepository extends JpaRepository<Favorito, Integer> {  
+
+}
