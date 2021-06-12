ce7f56c16dfc16d6c324dc4fc4af9db7ddc9a037
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-12 09:23:58 -0300

    add decorators na classe Favorito

--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Favorito.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/model/Favorito.java
@@ -1,13 +1,36 @@
 package edu.ifes.ci.si.les.ec.model;
 
-public class Favorito {
+import java.io.Serializable;
+import javax.validation.constraints.*;
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class Favorito implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	private Integer id;
 
+	@NotNull(message = "O Usuário deve ser preenchido")
+	@ManyToOne()
+	@JoinColumn(name="livro_id")
 	private Usuario usuario;
 
+	@NotNull(message = "O Livro deve ser preenchido")
+	@ManyToOne()
+	@JoinColumn(name="livro_id")
 	private Livro livro;
 
+	@NotNull(message = "O Escritor deve ser preenchido")
+	@ManyToOne()
+	@JoinColumn(name="escritor_id")
 	private Escritor[] escritor;
-
 }
