0a1abef7d8ada4f489282e7a43322a8d6b192f20
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-12 15:45:33 -0300

    corrigindo DbService

--- codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/_DBService.java
+++ codigos/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/_DBService.java
@@ -6,6 +6,8 @@ import java.text.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.ifes.ci.si.les.ec.model.Livro;
+
 import edu.ifes.ci.si.les.ec.repositories.LivroRepository;
 
 @Service
