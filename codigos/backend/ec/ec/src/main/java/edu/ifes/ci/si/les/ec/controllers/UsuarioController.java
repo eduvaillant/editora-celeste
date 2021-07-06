cdd2ccd899dfc034ea0528bb1f2bf77043d5c32c
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-06 15:44:30 -0300

    add rota para inserir um novo usuario

---
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/controllers/UsuarioController.java
@@ -0,0 +1,31 @@
+package edu.ifes.ci.si.les.ec.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ifes.ci.si.les.ec.model.Usuario;
+import edu.ifes.ci.si.les.ec.services.UsuarioService;
+import edu.ifes.ci.si.les.ec.services.exceptions.ConstraintException;
+
+@RestController()
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+  @Autowired
+  private UsuarioService service;
+  
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Usuario> insert(@Valid @RequestBody Usuario obj, BindingResult br) {
+      if (br.hasErrors())
+        throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
+      obj = service.insert(obj);
+      return ResponseEntity.ok().body(obj);
+  }
+}
---
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/UsuarioService.java
@@ -0,0 +1,25 @@
+package edu.ifes.ci.si.les.ec.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import edu.ifes.ci.si.les.ec.model.Usuario;
+import edu.ifes.ci.si.les.ec.repositories.UsuarioRepository;
+import edu.ifes.ci.si.les.ec.services.exceptions.DataIntegrityException;
+
+@Service
+public class UsuarioService {
+  
+  @Autowired
+  private UsuarioRepository repository;
+
+  public Usuario insert(Usuario obj) {
+    obj.setId(null);
+      try {
+        return repository.save(obj);
+      } catch (DataIntegrityViolationException e) {
+          throw new DataIntegrityException("Campo(s) obrigatório(s) do Funcionário não foi(foram) preenchido(s): Bairro");
+      }
+  }
+}
