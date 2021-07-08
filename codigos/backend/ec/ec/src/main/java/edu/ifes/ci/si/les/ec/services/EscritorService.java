6b90a3802e9ef73c571ea90b246baa7514d3e38d
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-08 16:27:16 -0300

    add metodos no service do escritor

--- codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/EscritorService.java
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/EscritorService.java
@@ -1,8 +1,10 @@
 package edu.ifes.ci.si.les.ec.services;
 
+import java.util.Collection;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import edu.ifes.ci.si.les.ec.model.Escritor;
@@ -31,5 +33,27 @@ public class EscritorService {
       } catch (NoSuchElementException e) {
         throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Escritor.class.getName());
       }
+  }
+
+  public Collection<Escritor> findAll() {
+    return repository.findAll();
+  } 
+
+  public Escritor update(Escritor obj) {
+    findById(obj.getId());
+    try {
+      return repository.save(obj);
+    } catch (DataIntegrityViolationException e) {
+        throw new DataIntegrityException("Campo(s) obrigatório(s) do Escritor não foi(foram) preenchido(s): Bairro");
+    }
+  }
+
+  public void delete(Integer id) {
+    findById(id);
+    try {
+        repository.deleteById(id);
+    } catch (DataIntegrityViolationException e) {
+        throw new DataIntegrityException("Não é possível excluir este Escritor!");
+    }
   }
 }
