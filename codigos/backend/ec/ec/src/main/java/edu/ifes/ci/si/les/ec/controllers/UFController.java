97bf33be6c75fe9ee11bd62c7ea29396fa181b36
Elian Batista <elianbatista2000@gmail.com>
2021-07-10 14:40:05 -0300

    altera uf Controller

--- codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/controllers/UFController.java
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/controllers/UFController.java
@@ -1,3 +1,21 @@
+package edu.ifes.ci.si.les.ec.controllers;
+
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org;springframework.web.bind.annotation.RestController;
+
+import edu.ifes.ci.si.les.ec.model.UF;
+import edu.ifes.ci.si.les.ec.services.UFService;
+import edu.ifes.ci.si.les.ec.services.exceptons.ConstraintException;
+
 @RestController()
 @RequestMapping(value = "/uf")
 public class UFController {
