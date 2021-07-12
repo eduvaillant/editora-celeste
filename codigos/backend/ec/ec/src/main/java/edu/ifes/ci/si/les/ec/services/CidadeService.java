b7dcf208819dc55557b55b7d9e5e45a10a6c1145
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-12 14:04:45 -0300

    corrigindo erros de import

--- codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/CidadeService.java
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/services/CidadeService.java
@@ -1,7 +1,6 @@
 package edu.ifes.ci.si.les.ec.services;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import edu.ifes.ci.si.les.ec.model.Cidade;
+import edu.ifes.ci.si.les.ec.model.UF;
 import edu.ifes.ci.si.les.ec.repositories.CidadeRepository;
 import edu.ifes.ci.si.les.ec.services.exceptions.*;
 
@@ -22,7 +22,6 @@ public class CidadeService {
     try {
       return repository.save(obj);
     } catch (DataIntegrityViolationException e) {
-      // TODO corrigir mensagem
       throw new DataIntegrityException("Campo(s) obrigatório(s) da Cidade não foi(foram) preenchido(s)");  
     }
   }
