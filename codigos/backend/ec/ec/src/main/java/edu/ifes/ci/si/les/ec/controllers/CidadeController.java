2e2953456139fa34f4b74fa32522a962784f256d
Elian Batista <elianbatista2000@gmail.com>
2021-07-12 11:03:15 -0300

    finaliza criacao da CidadeService e CidadeController

--- codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/controllers/CidadeController.java
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/controllers/CidadeController.java
@@ -13,38 +13,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.ifes.ci.si.les.ec.model.UF;
-import edu.ifes.ci.si.les.ec.services.UFService;
+import edu.ifes.ci.si.les.ec.model.Cidade;
+import edu.ifes.ci.si.les.ec.services.CidadeService;
 import edu.ifes.ci.si.les.ec.services.exceptions.ConstraintException;
 
 @RestController()
-@RequestMapping(value = "/uf")
-public class UFController {
+@RequestMapping(value = "/cidade")
+public class CidadeController {
   @Autowired
-  private UFService service;
+  private CidadeService service;
 
   @RequestMapping(method = RequestMethod.POST)
-  public ResponseEntity<UF> insert(@Valid @RequestBody UF obj, BindingResult br) {
+  public ResponseEntity<Cidade> insert(@Valid @RequestBody Cidade obj, BindingResult br) {
     if (br.hasErrors())
       throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
     obj = service.insert(obj);
-    return ResponseEntity.ok().body(obj);
+    return ResponseEntity.ok().body(obj);  
   }
 
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public ResponseEntity<UF> find(@PathVariable Integer id) {
-    UF obj = service.findById(id);
+  public ResponseEntity<Cidade> find(@PathVariable Integer id) {
+    Cidade obj = service.findById(id);
     return ResponseEntity.ok().body(obj);
   }
 
   @RequestMapping(method = RequestMethod.GET)
-  public ResponseEntity<Collection<UF>> findAll() {
-    Collection<UF> collection = service.findAll();
+  public ResponseEntity<Collection<Cidade>> findAll() {
+    Collection<Cidade> collection = service.findAll();
     return ResponseEntity.ok().body(collection);
   }
 
   @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-  public ResponseEntity<UF> update(@Valid @RequestBody UF obj, BindingResult br) {
+  public ResponseEntity<Cidade> update(@Valid @RequestBody Cidade obj, BindingResult br) {
     if (br.hasErrors())
       throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
     obj = service.update(obj);
