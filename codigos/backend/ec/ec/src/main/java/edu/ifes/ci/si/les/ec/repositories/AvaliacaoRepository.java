36cf04b4bfe4007d656cfd0cc120f890d9389333
Elian Batista <elianbatista2000@gmail.com>
2021-09-08 17:39:46 -0300

    resolve erros da avaliacao repository

--- codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/repositories/AvaliacaoRepository.java
+++ codigos/backend/ec/ec/src/main/java/edu/ifes/ci/si/les/ec/repositories/AvaliacaoRepository.java
@@ -3,6 +3,7 @@ package edu.ifes.ci.si.les.ec.repositories;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,10 +20,10 @@ public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Integer> {
    
     @Transactional(readOnly = true)
     @Query(value = "SELECT * from avaliacao av WHERE av.livro_id = ?1 order by nota asc limit 3;", nativeQuery = true)
-    public boolean pioresByLivroId(Integer livro_id);
+    public Collection<Avaliacao> pioresByLivroId(Integer livro_id);
 
     @Transactional(readOnly = true)
     @Query(value = "SELECT * from avaliacao av WHERE av.livro_id = ?1 order by nota desc limit 3;", nativeQuery = true)
-    public boolean melhoresByLivroId(Integer livro_id);
+    public Collection<Avaliacao> melhoresByLivroId(Integer livro_id);
 }
 
